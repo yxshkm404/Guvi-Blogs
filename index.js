a55db5c98de7b68c5dfd42f0266ef2408bf2f0fc
unknown <yaswanth.kumar.it@outlook.com>
2025-07-15 06:12:51 -0700

    intial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+//configrations
+
+//middlewares
+
+//routes
+
+//listener
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
