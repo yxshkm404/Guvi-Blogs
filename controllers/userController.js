c2482c03c212fb3818491fecc1899da7c2d56b7a
yxshkm404 <yaswanth.kumar.it@outlook.com>
2025-07-31 16:46:03 +0530

    fixed signup page

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,11 +8,13 @@ exports.handleUserSignup = async function(req, res) {
    try {
     if(!fullName) throw new Error('Full name is required');
     if(!email) throw new Error('Email is required');
-    if(!password || password.length > 5) throw new Error('Password is required and minimum length is 5 characters');
+   if (!password || password.length < 5) throw new Error('Password is required and minimum length is 5 characters');
+
     await User.create({fullName, email, password});
     return res.render('login', { success: 'Signup successful! Please login.' });
    }
    catch (err) {
     res.render('signup', { error: 'An error occurred during signup. Please try again.' });
+    console.log(err.message);
    }
 }
