3ecd88be7917fbeb213b04afe295db55ae94b66e
yxshkm404 <yaswanth.kumar.it@outlook.com>
2025-08-09 12:39:41 +0530

    multer added

--- routers/blogRoutes.js
+++ routers/blogRoutes.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 
-router.get("/create",function(req, res) {
-    res.render("createBlog")
-})
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public/uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null,file.originalname); 
+    }
+});
+
+const upload = multer({ storage });
+
+router.get("/create", function(req, res) {
+    res.render("createBlog");
+});
+
+router.post("/create", upload.single("coverImage"), function(req, res) {
+    console.log(req.body); // text inputs
+    console.log(req.file); // uploaded file info
+    return res.render("createBlog");
+});
 
 module.exports = router;
